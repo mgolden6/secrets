86323bb3d50e5133580f81dc667b30a7580b035c
Golden <goldenm4@BKSQ-80PD-MAC.hsd1.pa.comcast.net>
2019-08-18 22:45:39 -0400

    prepared starting files with npm and .gitignore

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 //jshint esversion:6
+//require modules
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+//configure express
+const port = 3000;
+const app = express();
+
+//configure ejs
+app.set("view engine", "ejs");
+
+//configure body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//configure access to local files
+app.use(express.static("public"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//test express server
+app.listen(port, function () {
+    console.log("Server started on port " + port);
+});
