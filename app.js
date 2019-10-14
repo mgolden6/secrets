2a04b207349fa4eb39a84191a09acb1235654d6a
Michael <mgolden6@gmail.com>
2019-10-14 14:21:02 -0400

    change order of port selection to remote before local

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const secretRoutes = require("./routes/secret-routes");
 const bodyParser = require("body-parser");
 
 // configure express
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // set up view engine (ejs)
